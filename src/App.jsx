08a5feae8b1ac3906cbc9c43531be2d5142cf4d6
dorcasgold <odetayodorcas123@gmail.com>
2024-10-21 15:55:13 +0100

    demo

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import AppLayout from "./layouts/AppLayout"
+import LandingPage from "./pages/LandingPage"
+import Onboarding from "./pages/Onboarding"
+import JobListing from "./pages/JobListing"
+import JobPage from "./pages/JobPage"
+import PostJob from "./pages/PostJob"
+import SavedJobs from "./pages/SavedJobs"
+import MyJobs from "./pages/MyJobs"
+import { ThemeProvider } from "./components/theme-provider"
+import ProtectedRoute from "./components/protected-route"
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  )
+}
+
+export default App
